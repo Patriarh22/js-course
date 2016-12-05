1eefebe42e62fd1da04721372a64dfca90279221
volodymyr.kravchenko <volodymyr.kravchenko@masterofcode.com>
2016-12-05 14:04:55 +0200

    Homework modifications

--- homeworks/volodymyr.kravchenko_Patriarh22/homework_3/js/main.js
+++ homeworks/volodymyr.kravchenko_Patriarh22/homework_3/js/main.js
@@ -2,27 +2,41 @@
 
     console.log(this);
 
-    this.setTimeout = function(delay, callback){
-        var date = Date.now();
-        while(Date.now() < date + delay){}
-        try {
-            callback.call();
-        }
-        catch (exception) {
-            console.log('=====Error====');
-            console.log(exception);
-        }
+    window.realTimeOut = window.setTimeout;
+
+    window.setTimeout = function (delay, callback) {
+        return window.realTimeOut(function () {
+            try {
+                callback();
+            }
+            catch (exeption){
+                console.log('========error========');
+            }}, delay)
     };
 
-    this.setInterval = function(callback, delay){
-        this.setTimeout(delay, callback);
-        this.setInterval(callback, delay);
+
+    // window.setTimeout = function(delay, callback){
+    //     var date = Date.now();
+    //     while(Date.now() < date + delay){}
+    //     try {
+    //         callback.call();
+    //     }
+    //     catch (exception) {
+    //         console.log('=====Error====');
+    //         console.log(exception);
+    //     }
+    // };
+
+    window.setInterval = function(callback, delay){
+        window.setTimeout(delay, function(){callback()});
+        window.setInterval(function(){callback()}, delay);
     };
 
     function timeOutCallback(){
-        alert('======TimeOut========');
+        alert('=====TimeOut Done====');
     }
 
-    this.setInterval(function (){timeOutCallback()}, 2000);
+    setInterval(function (){timeOutCallback()}, 2000);
+    // setTimeout(5000, function (){timeOutCallback()});
 
 })();
